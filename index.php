430da42060ef52d380f970eb10e1d62f92d37c0f
cjr <cjr@zju.edu.cn>
2013-09-09 22:28:26 +0800

    only a header, roughly no php written

--- index.php
+++ index.php
@@ -3,24 +3,47 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="content-type" content="text/html">
-	<!-- for compatibility of IE -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<!-- for mobile devices, no zooming -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-	<!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 	
 	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="js/html5shiv.js"></script>
       <script src="js/respond.min.js"></script>
     <![endif]-->
+	
+	<style>
+		body{
+			font-family: "微软雅黑", sans-serif;
+		}
+		.container{
+			max-width: 600px;
+			padding-left: 0;
+			padding-right: 0;
+		}
+		#title{
+			background-color:#EDA948;
+			padding: 15px 15px;
+			font-size: 24px;
+		}
+		#navi{
+			background-color:#C3A979;
+			padding: 5px 15px;
+		}
+	</style>
 </head>
 <body>
-	<h1>hello</h1>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
+	<div class="container">
+		<div id="header">
+			<div id="title">一餐易餐 - 手机版</div>
+			<div id="navi">首页|我的订单</div>
+		</div>
+		<h1>hello</h1>
+		<button type="button" class="btn btn-default">hello</button>
+	</div>
+	
+    <script src="js/jquery-1.10.2.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
